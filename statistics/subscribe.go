01a0df91da5998c97c0b3ec84ec68ae1c0fc128a
pavel <p.baranushkin@gmail.com>
2023-08-13 22:20:08 +0400

    refactor: file usage statistics

--- statistics/subscribe.go
+++ statistics/subscribe.go
@@ -2,31 +2,22 @@ package statistics
 
 import "fmt"
 
-func (c *Client) Subscribe(handler func(fileId string, inUse bool) (ok bool)) error {
-	stats, err := c.channel.Consume(c.queue.Name, "", false, false, false, false, nil)
+func (c *Client) Subscribe(handler func(fileId string, inUse bool)) error {
+	stats, err := c.channel.Consume(c.queue.Name, "", true, false, false, false, nil)
 
 	if err != nil {
 		return fmt.Errorf("failed to subscribe on files usage statistics: %w", err)
 	}
 
 	go func() {
-		var sErr error
 		info := FileUsage{}
 
 		for s := range stats {
-			sErr = info.deserialize(s.Body)
-
-			if sErr != nil {
-				s.Reject(false)
+			if info.deserialize(s.Body) != nil {
 				continue
 			}
 
-			if handler(info.FileId, info.InUse) {
-				s.Ack(false)
-				continue
-			}
-
-			s.Reject(true)
+			handler(info.FileId, info.InUse)
 		}
 	}()
 
